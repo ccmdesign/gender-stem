4779306492a356ec866bf897ff10b92f53369aa2
Seidgy Amano <seidgy@gmail.com>
2025-01-28 15:55:45 -0300

    Alteracoes para ter um arquivo de layout único para os idiomas

--- src/_data/languages.js
+++ src/_data/languages.js
@@ -1,6 +1,9 @@
 export default {
     en: {
       dir: '',
+      title: 'Gender STEM',
+      brow: 'Brow in English',
+      description: 'Description in English',
       availableText: 'This page is also available in:',
       skipText: 'Jump to main content',
       ariaPrimary: 'Primary',
@@ -12,6 +15,9 @@ export default {
    
     es: {
       dir: '',
+      title: 'Género STEM',
+      brow: 'Ceja en español',
+      description: 'Descripción en español',
       availableText: 'Esta página también está disponible en:',
       skipText: 'Saltar al contenido principal',
       ariaPrimary: 'Primario',
